fda46524d4ecbd8b2d7c3b47e131402778bc5ffc
tmsnvl95 <tmsnvl95@naver.com>
2022-08-24 23:12:50 +0900

    0824

---
+++ 0823_화/14667.py
@@ -0,0 +1,20 @@
+
+import sys
+sys.stdin = open("14467_input.txt")
+
+cow = [[-1]*10, [0]*10]
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+
+    if cow[0][n-1] == -1:
+        cow[0][n-1] = m
+        continue
+    if cow[0][n-1] != m:
+        cow[0][n-1] = m
+        cow[1][n-1] += 1
+        continue
+
+print(sum(cow[1]))
