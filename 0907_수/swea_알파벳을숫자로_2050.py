1cad06fb4bab683abfdacb032233e1883dff8574
tmsnvl95 <tmsnvl95@naver.com>
2022-09-08 11:47:29 +0900

    0907_3

---
+++ 0907_수/swea_알파벳을숫자로_2050.py
@@ -0,0 +1,12 @@
+# 알파벳으로 이루어진 문자열을 입력 받아 각 알파벳을 1부터 26까지 숫자로 변환하여
+# 출력하라
+
+import sys
+sys.stdin = open("input.txt")
+
+n = input()
+
+for i in range(len(n)):
+    print(ord(n[i])-64, end=' ')
+
+# print(n, type(n))
