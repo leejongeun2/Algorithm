fdbd7d6eb64feb2be286ceb45724b4e3fc00b79c
tmsnvl95 <tmsnvl95@naver.com>
2022-09-14 01:02:30 +0900

    0913

--- 0911_일/1302_베스트셀러.py
+++ 0911_일/1302_베스트셀러.py
@@ -1,24 +1,22 @@
-t = int(input())
+t = int(input()) # 입력 갯수 입력 받고 
 
-cnt = {}
+cnt = {} # 빈 딕셔너리 생성
 
-for i in range(t):
+for i in range(t): # 입력 갯수만큼 반복
     w = input()
     if w in cnt:
         cnt[w] += 1
     else:
         cnt[w] = 1
 
-max1 = max(cnt.values())
+max1 = max(cnt.values()) # 딕셔너리의 값 중에 가장 큰 숫자
 
 best = []
 
 for a, b in cnt.items():
-    
+    if b == max1:
+        best.append(a)
 
-if cnt.count(max(cnt.keys())) > 1:
-    print(cnt)
-else:
-    print(max(cnt.keys()))
+print(sorted(best)[0])
 
     
