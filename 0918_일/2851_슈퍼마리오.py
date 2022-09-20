6ca16194ac0ea41d43cd0fd0bc678a5ba4aeccd4
tmsnvl95 <tmsnvl95@naver.com>
2022-09-20 09:29:07 +0900

    0919

--- 0918_일/2851_슈퍼마리오.py
+++ 0918_일/2851_슈퍼마리오.py
@@ -11,9 +11,10 @@ for i in m:
     result += i # result 변수에 입력 값을 더해주기
     if result >= 100: # 더해준 것들이 100이 넘는다는 것들 안에서
         if abs(result-100) <= abs(100-(result-i)): 
-# 딱 result값이 100이 었을 때, 0 <= 100-(100-40) => 40
+# 예를 들어 result값이 100이 었을 때, 0 <= 100-(100-40) => 40
+# 즉, 바로 전 것(전 값까지의 합)과의 차이를 비교해서 현재 차가 더 적다면 멈춤 
             break
         else:
-            result -= i # 아닌 경우, 다음 것을 뺀다,
+            result -= i # 아닌 경우, 전 것을 출력
             break
 print(result)
