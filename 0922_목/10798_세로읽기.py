6196d3d8b528a96dafd4d9fdab999383d2bb241d
tmsnvl95 <tmsnvl95@naver.com>
2022-09-24 20:06:46 +0900

    0923

--- 0922_목/10798_세로읽기.py
+++ 0922_목/10798_세로읽기.py
@@ -8,8 +8,7 @@ for _ in range(5):
     w = input()
     lis.append(w)
     word.append(len(w))
-print(lis)
-print(word)
+
 
 rst = ''
 # for i in lis:
@@ -17,7 +16,7 @@ rst = ''
 #         print(j, end="")
 for i in range(max(word)): # 길이가 최대 긴 것만큼 반복
     for j in range(5): # 5줄(5 단어)이니까 5번 반복
-        if i < word[j]: # 첫번쨰 단어길이가 0보다 더 크면
+        if i < word[j]: # 해당하는 세로줄 인덱스 < 해당하는 자릿수라면 즉, 5번째 세로줄(인덱스4) < 6 해당되며,5번째 세로줄(인덱스4) < 4
             rst += lis[j][i] #
 print(rst)
 
