6d8ff1644d154a3af31a9429ebbdf24007816041
tmsnvl95 <tmsnvl95@naver.com>
2022-09-29 11:32:21 +0900

    0929_2669풀이추가

--- Baekjoon/Bronze/1/2669_직사각형네개.py
+++ Baekjoon/Bronze/1/2669_직사각형네개.py
@@ -14,13 +14,13 @@ for i in range(4):
     i, j, x, y = map(int, input().split())
     
     
-    for a in range(i, x): 
+    for a in range(i, x): # 박스가 밑에서부터 위로 올라감
         for b in range(j, y):
-            matrix[a][b] = 1
+            matrix[a][b] = 1 # 겹치는 부분(이미 있는 1이 있는 것)은 1로 유지할 수 있도록
 
 answer = 0
 for row in matrix:
-    answer += sum(row)
+    answer += sum(row) # 각 배열(세로줄)의 합을 구해서 변수에 순차적 저장
 
 print(answer)
 
