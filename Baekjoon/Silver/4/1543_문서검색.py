1ed2877c94f936c8727c4c155292633b1d41f4c9
tmsnvl95 <tmsnvl95@naver.com>
2022-10-04 10:08:52 +0900

    1003

--- Baekjoon/Silver/4/1543_문서검색.py
+++ Baekjoon/Silver/4/1543_문서검색.py
@@ -1,1 +1,13 @@
 # 1004 아침 풀 예정
+
+doc = input()
+word = input()
+count = 0
+i = 0
+while i <= len(doc) - len(word):
+    if doc[i:i + len(word)] == word:
+        count += 1
+        i += len(word) # 단어의 길이만큼 인덱스를 더해주고
+    else:              # 찾지 못하면
+        i += 1         # 1만큼 인덱스를 더해줌
+print(count)
